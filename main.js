a97bb973652db5dccf7df7d639ce814e1de2b444
Alvarez Jesica <alvarezjs1990@gmail.com>
2024-01-13 21:49:26 -0300

    Se conecto la API Random User Generator y se empezo a trabajar en el buscador

---
+++ main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+      
+    const userListContainer = document.getElementById('user-list');
+
+    fetch('https://randomuser.me/api/?results=8&nat=us&inc=name,picture,gender,dob,location')
+        .then(response => response.json())
+        .then(data => {
+            
+            const users = data.results;
+            users.forEach(user => {
+                const userCard = createUserCard(user);
+                userListContainer.appendChild(userCard);
+            });
+        });
+   
+    function createUserCard(user) {
+        const card = document.createElement('article');
+        card.classList.add('slider-content');
+   
+        card.innerHTML = `
+            <div class="card-wrapper">
+                <div class="card">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                        <img loading="lazy" src="${user.picture.large}" alt="User Thumbnail" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h1 class="name">${user.name.first} ${user.name.last}</h1>
+                        <p class="description">Género: ${user.gender}</p>
+                        <p class="description">Fecha de Nacimiento: ${user.dob.date}</p>
+                        <p class="description">Dirección: ${user.location.city}, ${user.location.country}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        return card;
+    }
+});
+
